de77b399d417a30faeba3cb5c9f7f6fec5a1557d
Ayumu <tommy.tommy.walker@gmail.com>
2021-03-15 00:26:44 +0900

    Make tree locations scripts

---
+++ soma_mapping/scripts/make_tree_locations.py
@@ -0,0 +1,31 @@
+import numpy as np
+from tkinter import filedialog
+import glob
+import os
+import open3d as o3d
+
+ROOT_DIR = '/mnt/d/bagfiles/hdl_graph_slam_exp/map_organize'
+TREE_LOCATIONS='/home/hayashi/catkin_ws/src/soma_pkg/soma_mapping/data/tree_locations.txt'
+
+if __name__ == '__main__':
+    print('Make tree locations file')
+
+    dir_path = filedialog.askdirectory(initialdir=ROOT_DIR)
+    print('select dir:', dir_path)
+
+    files=glob.glob(dir_path+'/*.txt')
+    print(len(files))
+
+    center_points = []
+    for file in files: #absolute paths
+      cloud = o3d.io.read_point_cloud(file,format='xyz')
+      # print(cloud)
+      # print(cloud.get_center())
+      center_points.append(cloud.get_center())
+    
+    center_points = np.array(center_points)
+    print(center_points)
+    np.savetxt(TREE_LOCATIONS,center_points,fmt='%.3f',delimiter='\t')
+
+
+
