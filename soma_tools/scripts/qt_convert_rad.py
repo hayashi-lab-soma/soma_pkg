30b61395e706b33bae848746f4701f7b990517b4
takumitomokawa <tomokawa.takumi163@mail.kyutech.jp>
2021-03-21 03:28:08 +0900

    add maxon_epos_msg topics

--- soma_tools/scripts/qt_convert_rad.py
+++ soma_tools/scripts/qt_convert_rad.py
@@ -4,7 +4,9 @@ import rospy
 import rosparam
 import sys
 import math
-from std_msgs.msg import Float32
+from std_msgs.msg import Float64
+from maxon_epos_msgs.msg import MotorStates
+from maxon_epos_msgs.msg import MotorState
 
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel
 
@@ -13,24 +15,39 @@ window = QWidget()
 window.setWindowTitle('Click Sample')
 layout = QVBoxLayout()
 button = QPushButton('Publish')
-edit = QLineEdit('')
-label = QLabel('')
+steer_edit = QLineEdit('')
+rear_edit = QLineEdit('')
+front_edit = QLineEdit('')
+throttle_edit = QLineEdit('')
 
-layout.addWidget(edit)
-layout.addWidget(label)
+layout.addWidget(steer_edit)
+layout.addWidget(rear_edit)
+layout.addWidget(front_edit)
+layout.addWidget(throttle_edit)
 layout.addWidget(button)
 window.setLayout(layout)
 
 def converter():
-    deg = float(edit.text())
-    rad = math.radians(deg)
-
     rospy.init_node('conoverter', anonymous=True)   
-    pub = rospy.Publisher('radian', Float32, queue_size=1)
+    pub = rospy.Publisher('radian', MotorStates, queue_size=1)
 
+    steer_rad = math.radians(float(steer_edit.text()))
+    rear_rad = math.radians(float(rear_edit.text()))
+    front_rad = math.radians(float(front_edit.text()))
+    throttle_rad = math.radians(float(throttle_edit.text()))
+
+    motor_cmd = MotorStates()
+    motor_cmd.states.append(MotorState(position=steer_rad))
+    motor_cmd.states.append(MotorState(position=rear_rad))
+    motor_cmd.states.append(MotorState(position=front_rad))
+    motor_cmd.states.append(MotorState(position=throttle_rad))
+
+
+    motor_cmd.header.stamp = rospy.Time.now()
     r = rospy.Rate(1)
-    rospy.loginfo(rad)
-    pub.publish(rad)
+    rospy.loginfo("published successfully")
+    pub.publish(motor_cmd)
+    r.sleep()
 
 def main():
     window.show()
@@ -38,10 +55,6 @@ def main():
     def on_button_clicked():
         converter()
     button.clicked.connect(on_button_clicked)
-
-    def on_edit_change(value):
-        label.setText(value)
-    edit.textChanged.connect(on_edit_change)
     app.exec_()
 
 if __name__ == '__main__':
