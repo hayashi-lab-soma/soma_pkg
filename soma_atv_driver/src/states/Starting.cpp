867654940e5e47a5dfb1619ddc6fe71971f22ff1
Ayumu <tommy.tommy.walker@gmail.com>
2021-03-20 16:23:17 +0900

    Auto stash before merge of "devel" and "origin/devel"

--- soma_atv_driver/src/states/Starting.cpp
+++ soma_atv_driver/src/states/Starting.cpp
@@ -10,23 +10,31 @@ Starting::~Starting()
 
 int Starting::_Transition(Definitions_t *data)
 {
-  if(abs(data->cmd_v) <= 0.001){
+  if (abs(data->cmd_v) <= 0.001)
+  {
     return State::Braking;
   }
   return State::Starting;
 }
 int Starting::_Enter(Definitions_t *data)
 {
-  if(data->cmd_v > 0.001){
+  if (data->cmd_v > 0.001)
+  {
     data->clutch_cmd = Clutch::Forward;
   }
-  else if(data->cmd_v < 0.001){
+  else if (data->cmd_v < 0.001)
+  {
     data->clutch_cmd = Clutch::Reverse;
   }
 
   // data->target_positions[0] =
   // data->target_velocity[1] =
   // data->target_velocity[3] =
+<<<<<<< Updated upstream
+=======
+  data->target_positions[2] = Motor::FrontBrake::Min; //front brake
+  data->target_positions[3] = 9.0;                    //throttle (deg)
+>>>>>>> Stashed changes
 }
 
 int Starting::_Process(Definitions_t *data)
@@ -35,6 +43,7 @@ int Starting::_Process(Definitions_t *data)
     return 0;
 
   //rear brake open (slowly)
+  data->target_velocity[1] = 500; //rpm
   data->target_positions[1] = Motor::RearBrake::Min;
   data->target_positions[2] = Motor::FrontBrake::Min; //front brake
   data->target_positions[3] = 9.0; //throttle (deg)
