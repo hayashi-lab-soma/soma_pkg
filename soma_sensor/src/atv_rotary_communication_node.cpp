a39058c03717e11da88ef8ddaf28648f28f99585
Ayumu <tommy.tommy.walker@gmail.com>
2021-03-21 00:08:39 +0900

    fixed atv rotary node, version 1

--- soma_sensor/src/atv_rotary_communication_node.cpp
+++ soma_sensor/src/atv_rotary_communication_node.cpp
@@ -16,10 +16,58 @@ int main(int argc, char **argv)
   int send_port = pnh.param<int>("send_port", 223457);
   int recv_port = pnh.param<int>("recv_port", 223456);
 
+  //publisher
+  ros::Publisher pub_wheel_vel = nh.advertise<std_msgs::Float32>("/soma/wheel_vel", 3);
+
   //UDP sockets
   QUdpSocket send_sock;
   QUdpSocket recv_sock;
   recv_sock.bind(recv_port);
+
+  //recieve data struct
+  struct Recv_t
+  {
+    long pulse[2];
+    double v;
+    double d[2];
+    double T;
+  } recv;
+  recv.pulse[0] = 0;
+  recv.pulse[1] = 1;
+  recv.v = 0.0;
+  recv.d[0] = 0.0;
+  recv.d[1] = 0.0;
+  recv.T = 0.0;
+
+
+  ros::Rate rate(1.0/timer_t);
+  while (ros::ok())
+  {
+    // recieve process
+    if (recv_sock.waitForReadyRead(33))
+    {
+      while (recv_sock.bytesAvailable() > 0)
+      {
+        recv_sock.readDatagram((char *)&recv,
+                               sizeof(Recv_t));
+      }
+    }
+    else
+    {
+    }
+
+
+
+    //publish
+    std_msgs::Float32 wheel_vel;
+    wheel_vel.data = recv.v;
+    pub_wheel_vel.publish(wheel_vel);
+
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  ROS_INFO("Shutdown ATV rotary encoder communication node");
 
   return 0;
 }
