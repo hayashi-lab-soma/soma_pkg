4bd6bd5e2542966e3fbbef58d7baae981425bb88
Ayumu <tommy.tommy.walker@gmail.com>
2021-04-11 04:52:26 +0900

    add global map publisher from pcd file

--- soma_samples/src/sample_local_costmap.cpp
+++ soma_samples/src/sample_local_costmap.cpp
@@ -7,11 +7,14 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "local_costmap");
   ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+
+  std::string costmap_ns = pnh.param<std::string>("costmap_ns", "costmap");
 
   tf2_ros::Buffer *tfBuf = new tf2_ros::Buffer();
   tf2_ros::TransformListener *tfListener = new tf2_ros::TransformListener(*tfBuf);
 
-  costmap_2d::Costmap2DROS costmap("local_costmap_rgbd_front", *tfBuf);
+  costmap_2d::Costmap2DROS costmap(costmap_ns, *tfBuf);
   costmap.resetLayers();
   costmap.start();
 
