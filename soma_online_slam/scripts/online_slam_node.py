0b976fc3ed2758f913932c7e1579e6b30cf0d204
Sylvain Geiser <geiser.nathan-sylvain778@mail.kyutech.jp>
2021-10-27 16:43:08 +0200

    Improve display

--- soma_online_slam/scripts/online_slam_node.py
+++ soma_online_slam/scripts/online_slam_node.py
@@ -3,67 +3,96 @@
 import rospy
 import numpy as np
 from geometry_msgs.msg import PoseArray
-from std_msgs.msg import Float64MultiArray
 from math import sqrt, atan2
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from onlineSLAMSolver import OnlineSLAMSolver, Particle
+from onlineSLAMSolver import OnlineSLAMSolver
 
 
-class Visualiser:
+class OnlineSLAMNode:
     def __init__(self):
+        self.solver = OnlineSLAMSolver(particles_num=10)
+
         self.fig, self.ax = plt.subplots()
-        self.ln0, = plt.plot([0], [0], 'ro', markersize=20)
-        self.ln1, = plt.plot([], [], 'bo', markersize=10)
-        self.x_data, self.y_data = [], []
+        self.particles_ln, = plt.plot([], [], 'ro', markersize=5)
+        self.features_ln, = plt.plot([], [], 'bo', markersize=10)
+        self.particles_x_data, self.particles_y_data = [], []
+        self.features_x_data, self.features_y_data = [], []
+
+        self.mutex = False
+
+        return
 
     def plot_init(self):
         self.ax.set_xlim(-20, 20)
         self.ax.set_ylim(-20, 20)
-        return self.ln1
+
+        return self.particles_ln, self.features_ln
+
+    def update_plot(self, frame):
+        if self.mutex:
+            return
+
+        print("DISPLAY\n")
+
+        self.particles_ln.set_data(
+            self.particles_x_data, self.particles_y_data)
+        self.features_ln.set_data(self.features_x_data, self.features_y_data)
+
+        return self.particles_ln, self.features_ln
 
     def observation_update(self, data):
+        if self.mutex:
+            print("Already running...\n")
+            return
+
+        self.mutex = True
+        print("SOLVER LOOP")
+
         observation = []
         for pose in data.poses:
             # Polar coordinates
-            r = sqrt(pose.position.x**2 + pose.position.y**2)
-            theta = atan2(pose.position.y, pose.position.x)
+            d = sqrt(pose.position.x**2 + pose.position.y**2)
+            phi = atan2(pose.position.y, pose.position.x)
 
-            observation += [r, theta]
+            observation.append(np.array([[d], [phi]]))
 
-        # Publish observation
-        # o = Float64MultiArray()
-        # o.data = observation
-        # observation_pub.publish(o)
+        self.solver.motion_update([0.1, 0.001])
+        self.solver.observation_update(observation)
 
-        # Display
-        l = len(data.poses)
-        x, y = [], []
-        for i in range(l):
-            x.append(data.poses[i].position.x)
-            y.append(data.poses[i].position.y)
+        self.particles_x_data, self.particles_y_data = [], []
+        for p in self.solver.particles:
+            self.particles_x_data.append(p.pose[0][0])
+            self.particles_y_data.append(p.pose[1][0])
 
-        self.x_data = x[:]
-        self.y_data = y[:]
+        highest_weight = 0
+        for p in self.solver.particles:
+            if p.weight > highest_weight:
+                highest_weight = p.weight
+        most_probable_particle = p.clone()
 
-    def update_plot(self, frame):
-        self.ln1.set_data(self.x_data, self.y_data)
-        return self.ln1
+        self.features_x_data, self.features_y_data = [], []
+        if highest_weight > 0:
+            for feature in most_probable_particle.features:
+                self.features_x_data.append(feature.pose[0][0])
+                self.features_y_data.append(feature.pose[1][0])
+
+        self.mutex = False
+
+        print("END\n")
+
+        return
 
 
 if __name__ == '__main__':
     rospy.init_node('online_slam', anonymous=True)
 
-    vis = Visualiser()
+    node = OnlineSLAMNode()
 
     rospy.Subscriber('/tree_pose_array', PoseArray,
-                     callback=vis.observation_update)
-    observation_pub = rospy.Publisher(
-        '/observation', Float64MultiArray, queue_size=1)
+                     callback=node.observation_update, queue_size=1)
 
-    solver = OnlineSLAMSolver(particles_num=100)
-
-    ani = FuncAnimation(vis.fig, vis.update_plot, init_func=vis.plot_init)
+    ani = FuncAnimation(node.fig, node.update_plot, init_func=node.plot_init)
     plt.show(block=True)
 
     rospy.spin()
