a503c4534395a72bebc67931dd7c7ddc721639a5
Sylvain Geiser <geiser.nathan-sylvain778@mail.kyutech.jp>
2022-04-13 11:14:57 +0200

    Wait all trees centers updated to publish

--- soma_mapping/scripts/tree_coordinates.py
+++ soma_mapping/scripts/tree_coordinates.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import PoseArray, Pose
+from jsk_recognition_msgs.msg import Int32Stamped
 # from pcl_msgs.msg import ModelCoefficients
 
 
@@ -10,104 +11,175 @@ def fuse_centers(index, pose):
     return
 
 
-def fuse_centers0(data):
-    fuse_centers(0, data.poses[0])
-    pub.publish(trees_centers)
+def cluster(data):
+    global cluster_num
+    cluster_num = data.data
 
     return
+
+
+def loop():
+    global cluster_num, updates, pose
+
+    update = True
+    for i in range(cluster_num):
+        update = update and updates[i]
+    if update:
+        pub.publish(trees_centers)
+
+        for i in range(cluster_num):
+            updates[i] = False
+            trees_centers.poses[i] = pose
+
+    return
+
+
+def fuse_centers0(data):
+    fuse_centers(0, data.poses[0])
+    global updates
+    updates[0] = True
+    loop()
+
+    return
+
 
 def fuse_centers1(data):
     fuse_centers(1, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[1] = True
+    loop()
 
     return
+
 
 def fuse_centers2(data):
     fuse_centers(2, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[2] = True
+    loop()
 
     return
+
 
 def fuse_centers3(data):
     fuse_centers(3, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[3] = True
+    loop()
 
     return
+
 
 def fuse_centers4(data):
     fuse_centers(4, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[4] = True
+    loop()
 
     return
+
 
 def fuse_centers5(data):
     fuse_centers(5, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[5] = True
+    loop()
 
     return
+
 
 def fuse_centers6(data):
     fuse_centers(6, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[6] = True
+    loop()
 
     return
+
 
 def fuse_centers7(data):
     fuse_centers(7, data.poses[0])
-    pub.publish(trees_centers)
-
+    global updates
+    updates[7] = True
+    loop()
     return
+
 
 def fuse_centers8(data):
     fuse_centers(8, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[8] = True
+    loop()
 
     return
+
 
 def fuse_centers9(data):
     fuse_centers(9, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[9] = True
+    loop()
 
     return
+
 
 def fuse_centers10(data):
     fuse_centers(10, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[10] = True
+    loop()
 
     return
+
 
 def fuse_centers11(data):
     fuse_centers(11, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[11] = True
+    loop()
 
     return
+
 
 def fuse_centers12(data):
     fuse_centers(12, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[12] = True
+    loop()
 
     return
+
 
 def fuse_centers13(data):
     fuse_centers(13, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[13] = True
+    loop()
 
     return
+
 
 def fuse_centers14(data):
     fuse_centers(14, data.poses[0])
-    pub.publish(trees_centers)
-
+    global updates
+    updates[14] = True
+    loop()
     return
+
 
 def fuse_centers15(data):
     fuse_centers(15, data.poses[0])
-    pub.publish(trees_centers)
+    global updates
+    updates[15] = True
+    loop()
 
     return
 
 
 if __name__ == '__main__':
+    cluster_num = 0
+    updates = 16*[False]
+
     trees_centers = PoseArray()
     trees_centers.header.frame_id = "velodyne"
     trees_centers.poses = []
@@ -119,10 +191,13 @@ if __name__ == '__main__':
     pose.orientation.y = 0
     pose.orientation.z = 0
     pose.orientation.w = 1
+
     for i in range(16):
         trees_centers.poses.append(pose)
 
     rospy.init_node('tree_coordinates', anonymous=True)
+
+    rospy.Subscriber('/clustering/cluster_num', Int32Stamped, callback=cluster, queue_size=1)
 
     rospy.Subscriber('/tree0', PoseArray,
                      callback=fuse_centers0, queue_size=1)
@@ -158,6 +233,5 @@ if __name__ == '__main__':
                      callback=fuse_centers15, queue_size=1)
 
     pub = rospy.Publisher("/trees_centers", PoseArray, queue_size=1)
-    
 
     rospy.spin()
